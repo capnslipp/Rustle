45fc6052838fdd6db4c5cd920d51f9416686f0a6
Slipp Douglas Thompson <git@slippyd.com>
2015-05-02 20:52:25 -0400

    Initial Xcode project (from template)
    * New Xcode 6.3 project: iOS › Application › Obj-C/SceneKit/Universal.

---
+++ GTCoreBluetoothDemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  GTCoreBluetoothDemo
+//
+
+//  Copyright (c) 2015 Cap'n Slipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
