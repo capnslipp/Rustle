fbc7be13dfd184ae08e4ae21e19eb4a2f67155e2
Slipp Douglas Thompson <git@slippyd.com>
2015-05-05 02:25:24 -0400

    Basic Core Data integration
    * Started with Core Data code & files from a new Xcode project with “Use Core Data” enabled.  
    	» The `….xcdatamodeld` left as-is for now.
    * Cleaned up the template-generated Core Data code to follow my normal Obj-C style:  
    	⁃ method `{`s on new lines  
    	⁃ `.`-syntax whenever possible and not intentionally mutating  
    	⁃ `[… new]` instead of `[[… alloc] init]`  
    	⁃ Early-outs for failures instead of nested `if { … }`s  
    	⁃ `(… == nil)`/`(… != nil)` checks for memory we own (ivars); `(!…)`/`(…)` for references that may be present or may not  
    	⁃ `dispatch_once()` usage  
    	⁃ `const` usage for configurable data  
    	⁃ `NSException`s for unrecoverable states instead of `NSError`s (Apple's recommended style, technically)
    * Fixed issues with finding the `.mom`/`.momd` and simplified the logic by switching to NSManagedObjectModel's `+ mergedModelFromBundles`.  
    	» Recommended approach by http://stackoverflow.com/questions/4536414/cant-find-momd-file-core-data-problems#4536668
    * Fixed inefficiency (and will-never-happen logic condition) in `- saveContext` by adding a new `- hasManagedObjectContext` helper, which doesn't try to create the context if it doesn't exist.  
    	» Apple's template code had and issue with creating the NSManagedObjectContext on backgrounding/termination via `- saveContext`'s `if (self.managedObjectContext != nil)`, which would've never been nil because calling `self.managedObjectContext` would create the context.

--- App/AppDelegate.h
+++ App/AppDelegate.h
@@ -2,11 +2,19 @@
 // @author: Slipp Douglas Thompson
 
 @import UIKit;
+@import CoreData;
 
 
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property(retain, nonatomic) UIWindow *window;
+
+// Core Data
+@property(copy, readonly, nonatomic) NSURL *applicationDocumentsDirectory;
+@property(retain, readonly, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property(retain, readonly, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property(retain, readonly, nonatomic) NSManagedObjectContext *managedObjectContext;
+- (void)saveContext;
 
 @end
--- App/AppDelegate.m
+++ App/AppDelegate.m
@@ -5,7 +5,16 @@
 
 
 
+NSString *const kCoreDataSQLiteExtensionlessFileame = @"GTCoreBluetoothDemo";
+
+NSArray *const kMainBundlePlaceholder = nil;
+
+
+
 @interface AppDelegate ()
+
+@property(assign, readonly, nonatomic) BOOL hasManagedObjectContext;
+
 @end
 
 
@@ -29,6 +38,7 @@
 /// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+	[self saveContext];
 }
 
 /// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
@@ -44,6 +54,101 @@
 /// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+	[self saveContext];
+}
+
+
+#pragma mark Core Data Stack
+
+@synthesize applicationDocumentsDirectory=_applicationDocumentsDirectory;
+@synthesize managedObjectModel=_managedObjectModel, persistentStoreCoordinator=_persistentStoreCoordinator, managedObjectContext=_managedObjectContext;
+
+/// The directory the application uses to store the Core Data store file. This code uses a directory named the same as the CFBundleIdentifier within the application's documents directory.
+- (NSURL *)applicationDocumentsDirectory
+{
+	static dispatch_once_t sOnceToken;
+	dispatch_once(&sOnceToken, ^{
+		_applicationDocumentsDirectory = [NSFileManager.defaultManager URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask].lastObject;
+		[_applicationDocumentsDirectory retain];
+	});
+	
+	return _applicationDocumentsDirectory;
+}
+
+/// The managed object model for the application. It is a fatal error for the application not to be able to find and load its model.
+- (NSManagedObjectModel *)managedObjectModel
+{
+	static dispatch_once_t sOnceToken;
+	dispatch_once(&sOnceToken, ^{
+		_managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:kMainBundlePlaceholder];
+		[_managedObjectModel retain];
+	});
+	
+	return _managedObjectModel;
+}
+
+/// The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it.
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+	static dispatch_once_t sOnceToken;
+	dispatch_once(&sOnceToken, ^{
+		_persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.managedObjectModel];
+		
+		NSURL *storeURL = [[self.applicationDocumentsDirectory
+			URLByAppendingPathComponent:kCoreDataSQLiteExtensionlessFileame]
+			URLByAppendingPathExtension:@"sqlite"];
+		if (!storeURL)
+			@throw [NSException exceptionWithName:NSInvalidArgumentException
+				reason:[NSString stringWithFormat:@"Unable to find app document with filename “%@”.",
+					[kCoreDataSQLiteExtensionlessFileame stringByAppendingPathExtension:@"sqlite"]]
+				userInfo:nil];
+		
+		NSError *pscError = nil;
+		NSPersistentStore *persistentStore = [_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&pscError];
+		if (!persistentStore)
+			@throw [NSException exceptionWithName:NSInternalInconsistencyException
+				reason:@"There was an error creating or loading the application's saved data."
+				userInfo:@{ NSUnderlyingErrorKey: pscError }];
+	});
+	
+	return _persistentStoreCoordinator;
+}
+
+/// The managed object context for the application (which is already bound to the persistent store coordinator for the application.)
+- (NSManagedObjectContext *)managedObjectContext
+{
+	static dispatch_once_t sOnceToken;
+	dispatch_once(&sOnceToken, ^{
+		NSPersistentStoreCoordinator *coordinator = self.persistentStoreCoordinator;
+		if (!coordinator)
+			return;
+		
+		_managedObjectContext = [NSManagedObjectContext new];
+		_managedObjectContext.persistentStoreCoordinator = coordinator;
+	});
+	
+	return _managedObjectContext;
+}
+/// Used to prevent creating the `managedObjectContext` as a side-effect when we only want to know if one has been created yet.
+- (BOOL)hasManagedObjectContext {
+	return (_managedObjectContext != nil);
+}
+
+- (void)saveContext
+{
+	if (!self.hasManagedObjectContext)
+		return;
+	NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+	
+	if (managedObjectContext.hasChanges) {
+		NSError *mocError = nil;
+		BOOL successfullySaved = [managedObjectContext save:&mocError];
+		
+		if (!successfullySaved)
+			@throw [NSException exceptionWithName:NSInternalInconsistencyException
+				reason:@"There was an error saving the application's data."
+				userInfo:@{ NSUnderlyingErrorKey: mocError }];
+	}
 }
 
 
