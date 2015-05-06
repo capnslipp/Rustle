b671316b95d093c112eed648c2df18ddfef02674
Slipp Douglas Thompson <git@slippyd.com>
2015-05-05 21:22:48 -0400

    Moved Core Data logic into new SavedDataManager
    * Created a new SavedDataManager for taking care of the Core Data logic formerly within AppDelegate.  
    	» Has all the expected `+ sharedManager` stuff.  
    	» Gained AppDelegate's `- managedObjectModel`, `- persistentStoreCoordinator`, `- managedObjectContext`, `- hasManagedObjectContext`, and `- saveContext` methods.  
    	» Responds to `UIApplicationDidEnterBackground`/`WillTerminateNotification` by calling `- saveContext` (still) via `- registerAppNotifications`/`- deregisterAppNotifications` helper methods.  
    	» No longer using `dispatch_once()` for these, since there could be multiple instances now.  Also, we weren't using static storage before, which is a no-no.  Oops.  
    * AppDelegate's `applicationDocumentsDirectory` renamed to `documentsDirectory`.  The former was just too redundant.
    * AppDelegate's `_documentsDirectory` ivar changed to be a `sDocumentsDirectory` static var.  
    	» `dispatch_once()` with ivars could lead to problems, and non-static once tokens are tricky.  More info here: http://khanlou.com/2013/11/using-dispatch_once-to-setup-instances-and-not-singletons/

--- App/AppDelegate.h
+++ App/AppDelegate.h
@@ -2,7 +2,6 @@
 // @author: Slipp Douglas Thompson
 
 @import UIKit;
-@import CoreData;
 
 
 
@@ -10,11 +9,6 @@
 
 @property(retain, nonatomic) UIWindow *window;
 
-// Core Data
-@property(copy, readonly, nonatomic) NSURL *applicationDocumentsDirectory;
-@property(retain, readonly, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property(retain, readonly, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-@property(retain, readonly, nonatomic) NSManagedObjectContext *managedObjectContext;
-- (void)saveContext;
+@property(copy, readonly, nonatomic) NSURL *documentsDirectory;
 
 @end
